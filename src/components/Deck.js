3408acda791772047091dda31bce3184eb12c9c3
Alex Godfrey <alexjamesgodfrey@gmail.com>
2021-02-13 11:22:32 -0500

    added info toast

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -1,14 +1,44 @@
 import { arrayTypeAnnotation } from '@babel/types';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import Toast from 'react-bootstrap/Toast';
+import ToastBody from 'react-bootstrap/ToastBody';
+import ToastHeader from 'react-bootstrap/ToastHeader';
 import { useSpring, animated } from 'react-spring';
 import { useDrag } from 'react-use-gesture';
 import Card from './Card.js';
 import '../styles/Deck.scss';
 
 const Deck = (array) => {
+    const [minutes, setMinutes] = useState(0);
+
+    const delay = ms => new Promise(res => setTimeout(res, ms));
+
+    const minutesFunction = async () => {
+        for (let i = 0; i < 20; i++){
+            await delay(60000);
+            setMinutes(minutes + 1);
+            console.log(minutes);
+        }
+    }
+
+    useEffect(() => {
+        minutesFunction();
+    })
+
 
     return (
         <div className="main-container">
+            <Toast className="info-toast">
+                <Toast.Header>
+                    <strong className="mr-auto">info</strong>
+                    <small>{minutes} minutes ago</small>
+                </Toast.Header>
+                <Toast.Body>a warm welcome from sierra. click on or hover over a piece to view its medium, 
+                    materials used, and other info.
+                    use the dropdown to select either, print, signed print, or original. 
+                    click buy to become a patron.
+                </Toast.Body>
+            </Toast>
             {
                 array.arr.map((card, key) => {
                     return <Card
