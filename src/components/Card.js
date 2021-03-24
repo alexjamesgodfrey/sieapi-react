3b1bb38f06bcce3a1670d2b7153e7695f5fa8bd7
Alex Godfrey <agod@Alexs-MacBook-Air.local>
2021-03-24 14:05:32 -0400

    added loading handling

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import PayPal from './PayPal.js'
+import Spinner from 'react-bootstrap/Spinner'
 import { useSpring, animated } from 'react-spring';
 import { motion } from "framer-motion";
 import Button from 'react-bootstrap/Button';
@@ -76,8 +77,8 @@ const Card = (props) => {
                 <motion.div onTap={touchFunc}>
                     <img className="image" src={props.image} onLoad={() => setLoaded(true)} />
                 </motion.div>
-            
-            {body ? 
+             
+            {(body && loaded) ? 
                 <motion.div animate={{ y: 180 }} className="body-container">
                     <div className="title-area">
                         <h1>{props.title}</h1>
@@ -100,25 +101,9 @@ const Card = (props) => {
 
                 : 
 
-                <motion.div animate={{ y: 0 }} className="body-container-up">
-                    <div className="title-area">
-                        <h1>{props.title}</h1>
-                        <DropdownButton id="drop" variant="info" title={type}>
-                            {props.hasPrint ? <Dropdown.Item as="button" onClick={() => setType('print')}>print</Dropdown.Item> : <Dropdown.ItemText>print sold out</Dropdown.ItemText>}
-                            {props.hasSignedPrint ? <Dropdown.Item as="button" onClick={() => setType('signed print')}>signed print</Dropdown.Item> : <Dropdown.ItemText>signed print s/o</Dropdown.ItemText>}
-                            {props.hasOriginal ? <Dropdown.Item as="button" onClick={() => setType('original')}>original</Dropdown.Item> : <Dropdown.ItemText>original s/o</Dropdown.ItemText>}
-                        </DropdownButton>
-                    </div>
-                    <h3 className="date">{props.date}</h3>
-
-                    {type === 'print' ? <h6 className="description">{props.soldPrint} / ∞ sold</h6> : <span></span>}
-                    {type === 'signed print' ? <h6 className="description">{props.soldSignedPrint} / 20 sold</h6> : <span></span>}
-                    {type === 'original' ? <h6 className="description">0 / 1 sold</h6> : <span></span>}
-
-                    {type === 'print' ? <Button id="buy-button" variant="outline-info" size="md">buy ${props.printPrice}</Button> : <span></span>}
-                    {type === 'signed print' ? <Button id="buy-button" variant="outline-info" size="md">buy ${props.signedPrintPrice}</Button> : <span></span>}
-                    {type === 'original' ? <Button id="buy-button" variant="outline-info" size="md">buy ${props.originalPrice}</Button> : <span></span>}
-                </motion.div>
+                <div animate={{ y: 0 }} className="body-container-up">
+                    <Spinner animation="border" variant="danger" />
+                </div>
             }
 
             <Modal show={showModal} onHide={hideMod}>
