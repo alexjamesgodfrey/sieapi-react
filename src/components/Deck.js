70f02d96510a3644caff2d43ef576668c4128173
Alex Godfrey <agod@Alexs-MacBook-Air.local>
2021-03-25 08:57:00 -0400

    header spacing changes and card height changes

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -1,46 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import Toast from 'react-bootstrap/Toast';
 import Card from './Card.js';
 import '../styles/Deck.scss';
 
 const Deck = (array) => {
-    //state
-    //toggles shhowing of toast
-    const [toast, setToast] = useState(true);
-    const toggleToast = () => setToast(false);
-    //set toast minutes
-    const [minutes, setMinutes] = useState(0);
-
-    //delat function for minutes
-    const delay = ms => new Promise(res => setTimeout(res, ms));
-
-    //adds one minute to toast every 1 minute (max 20)
-    const minutesFunction = async () => {
-        for (let i = 0; i < 20; i++){
-            await delay(60000);
-            setMinutes(minutes + 1);
-            console.log(minutes);
-        }
-    }
-
-    useEffect(() => {
-        minutesFunction();
-    })
-
 
     return (
         <div>
+
+        
         <div className="main-container">
-            <Toast show={toast} onClose={toggleToast} className="info-toast">
-                <Toast.Header>
-                    <strong className="mr-auto">welcome</strong>
-                    {minutes > 1 ? <small>{minutes} minutes ago</small> : <small>just now</small>}
-                </Toast.Header>
-                <Toast.Body id="toast-body">to my website. click on or hover over a piece to view its medium, 
-                    materials used, and other info.
-                    use the dropdown and buy buttons to become a patron
-                </Toast.Body>
-            </Toast>
             {
                 array.arr.map((card, key) => {
                     return <Card
@@ -64,7 +31,6 @@ const Deck = (array) => {
                     }
                 )
             }
-           
         </div>
         <p className='footer'>© 2021 Godfrey Corp.</p>
         </div>
