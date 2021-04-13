78df70c68dc62ecdb3d8221d94e8e007868978b4
Alex Godfrey <agod@Alexs-MacBook-Air.local>
2021-04-13 12:13:27 -0400

    updated about

--- src/About.js
+++ src/About.js
@@ -8,7 +8,13 @@ const About = () => {
             <div className="main">
                 <Header />
                 <div className="about">
-                    <h1>About Me</h1>
+                    <h1>about me</h1>
+                    <div className="FAQ">
+                        <h5>can you create a website like this for me to sell or showcase my artwork?</h5>
+                        <p>yes. alex godfrey can make you a website just like this one, help you get a domain like sierraapicella
+                            .com, and edit the design of the website to your liking. </p>
+                    </div>
+                    
                 </div>
             </div>
         </div>
