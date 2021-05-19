d0c7e1e6bd024d3c5f613d5b004a55bc03e30711
Alex Godfrey <alexjamesgodfrey@gmail.com>
2021-05-19 16:06:28 -0400

    update add page

--- client/src/Admin.js
+++ client/src/Admin.js
@@ -1,7 +1,7 @@
 import React, { useRef, useState, useEffect } from 'react'
 import { useAuth } from './contexts/AuthContext.js';
 import { useHistory } from 'react-router-dom';
-import { Form, Button } from 'react-bootstrap';
+import { Form, Button, Alert } from 'react-bootstrap';
 import app from './firebase.js';
 import SmallWork from './components/SmallWork'
 import './styles/Admin.scss'
@@ -13,7 +13,8 @@ export default function Admin() {
     const [mode, setMode] = useState('add')
     const [image, setImage] = useState();
     const [works, setWorks] = useState([])
-    const [warning, setWarning] = useState(false)
+    const [alert, setAlert] = useState('')
+    const [warning, setWarning] = useState('')
 
     const titleRef = useRef();
     const dateRef = useRef();
@@ -29,22 +30,33 @@ export default function Admin() {
     }
 
     const handleSubmit = async (e) => {
-        e.preventDefault();
-        const bod = `{
-            "image": "${image}",
-            "title": "${titleRef.current.value}",
-            "date": "${dateRef.current.value}",
-            "description": "${descriptionRef.current.value}",
-            "price": "${parseFloat(priceRef.current.value)}",
-            "sold": "${soldRef.current.value === 'true'}"
-        }`
-        console.log(bod);
-        const resp = await fetch('/api/creatework', {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: bod
-        })
-        console.log(resp)
+        setAlert('')
+        setWarning('')
+        if (!(parseFloat(priceRef.current.value))) {
+            return setWarning('must enter a number for price')
+        }
+        try {
+            e.preventDefault();
+            const bod = `{
+                "image": "${image}",
+                "title": "${titleRef.current.value}",
+                "date": "${dateRef.current.value}",
+                "description": "${descriptionRef.current.value}",
+                "price": "${parseFloat(priceRef.current.value)}",
+                "sold": "${soldRef.current.value === 'true'}"
+            }`
+            console.log(bod);
+            const resp = await fetch('/api/creatework', {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: bod
+            })
+            console.log(resp)
+            return setAlert('successfully uploaded')
+        } catch (error) {
+            return setWarning('failed. please talk to me or try again.')
+        }
+        
     }
 
     const uploadPicture = async (e) => {
@@ -74,15 +86,19 @@ export default function Admin() {
     }, [])
 
     return (
-        <div style={{margin: '30px'}}>
+        <div style={{ margin: '30px', width: '100vw' }}>
+            <div style={{ margin: '0px auto', width: '800px'}}>
             <h3>admin -- {currentUser.email}</h3>
             <div className="d-flex justify-content-between" style={{ width: '500px'}}>
                 {mode === 'add' ? <h4 style={{ color: '#ffc0cb'}}>add a work</h4> : <h4 onClick={() => setMode('add')}>add a work</h4>}
                 {mode === 'edit' ? <h4 style={{ color: '#ffc0cb'}}>edit a work</h4>: <h4 onClick={() => setMode('edit')}>edit a work</h4>}
                 {mode === 'delete' ? <h4 style={{ color: '#ffc0cb'}}>delete a work</h4>: <h4 onClick={() => setMode('delete')}>delete a work</h4>}
             </div>
-            {mode === 'add' && 
-                <Form className="w-50">
+            {mode === 'add' &&
+                <div>
+                {alert && <Alert variant="success">{alert}</Alert>}
+                { warning && <Alert variant="danger">{warning}</Alert>}
+                <Form className="w-50" onSubmit={(e) => handleSubmit(e)}>
                 <Form.Group id="title">
                     <Form.Label id="font-color">title</Form.Label>
                     <Form.Control type="text" ref={titleRef} required></Form.Control>
@@ -98,21 +114,27 @@ export default function Admin() {
                 </Form.Group>
                 <Form.Group id="description">
                     <Form.Label id="font-color">description</Form.Label>
-                    <Form.Control type="text" ref={descriptionRef} required></Form.Control>
-                </Form.Group>
-                <Form.Group id="price">
-                    <Form.Label id="font-color">price (enter number)</Form.Label>
-                    <Form.Control type="text" ref={priceRef} required></Form.Control>
-                </Form.Group>
-                <Form.Group id="sold">
-                    <Form.Label id="font-color">sold</Form.Label>
-                    <Form.Control as="select" ref={soldRef} required>
-                        <option>false</option>
-                        <option>true</option>    
-                    </Form.Control>
-                </Form.Group>
-                <Button id="loginbutton" className="w-100 mx-auto" variant="warning" type="submit" onClick={(e) => handleSubmit(e)}>add work</Button>
+                    <Form.Control as="textarea" rows={3} ref={descriptionRef} required></Form.Control>
+                    </Form.Group>
+                <div className="d-flex justify-content-between">
+                    <Form.Group id="price">
+                        <Form.Label id="font-color">price (enter number)</Form.Label>
+                        <div className="d-flex">
+                            <span style={{ fontSize: '28px', marginRight: '10px', color: '#ffc0cb' }}>$</span>
+                            <Form.Control style={{ width: '80px' }} type="text" ref={priceRef} placeholder={'145'} required></Form.Control>
+                        </div>
+                    </Form.Group>
+                    <Form.Group id="sold" style={{ width: '100px'}}>
+                        <Form.Label id="font-color">sold</Form.Label>
+                        <Form.Control as="select" ref={soldRef} required>
+                            <option>false</option>
+                            <option>true</option>    
+                        </Form.Control>
+                    </Form.Group>
+                </div>
+                <Button style={{ color: 'white', margin: '0px auto' }} className="w-50" variant="warning" type="submit">add work</Button>
                 </Form>
+                </div>
             }
             {mode === 'delete' &&
                 <div className="d-flex">
@@ -123,7 +145,8 @@ export default function Admin() {
                     })}
                 </div>
             }
-            <h3 id="forgot-link" onClick={handleLogout} style={{ marginTop: '50px'}}>logout</h3>
+            <h3 id="forgot-link" onClick={handleLogout} style={{ marginTop: '50px' }}>logout</h3>
+            </div>
         </div>
     )
 }
